5de5170799dafc3e35b838d7af4fed7682befb79
Jim-Huang <jim.huang@asuscloud.com>
2022-01-05 14:48:31 +0800

    add : add project

---
+++ Library-C/Library-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Library-C
+//
+//  Created by 黃佳俊 on 2022/1/5.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
